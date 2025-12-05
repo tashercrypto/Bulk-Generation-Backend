ae73e16bfc1aa1dffd0043140d7ef091e272f073
tashercrypto <154053732+tashercrypto@users.noreply.github.com>
2025-12-05 16:21:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -79,6 +79,9 @@ app.post("/edit-image", upload.array("images"), async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Backend запущено на порту 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Backend запущено на порту", PORT);
 });
+
